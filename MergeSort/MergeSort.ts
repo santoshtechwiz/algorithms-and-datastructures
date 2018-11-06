06c786b14f862702e7ab440b52758b3ff045e766
Santosh Kumar Singh <Santosh.Singh@XCHANGING.MY.LOCAL>
2018-11-06 12:42:47 +0800

    Added Merge Sort

---
+++ MergeSort/MergeSort.ts
@@ -0,0 +1,32 @@
+export function mergeSort(arr: Array<number>): Array<number> {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(left: Array<number>, right: Array<number>): Array<number> {
+  let i = 0;
+  let j = 0;
+  let result: Array<number> = [];
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+  while (i < left.length) {
+    result.push(left[i]);
+    i++;
+  }
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+  return result;
+}
---
+++ test/MergeSortTest.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { mergeSort } from "../MergeSort/MergeSort";
+
+describe("Merge Sort", () => {
+  it("Sort the array in ascending order", () => {
+    const arr = [4, 2, 1, -1, 10, -2, 9];
+    var result = mergeSort(arr);
+
+    expect(mergeSort(arr)[0]).to.equal(-2);
+    expect(mergeSort(arr)).to.deep.equal([-2, -1, 1, 2, 4, 9, 10]);
+  });
+});
