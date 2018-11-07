f8429e1bee9548764d1d371c6beee35babc66886
Santosh Kumar Singh <Santosh.Singh@XCHANGING.MY.LOCAL>
2018-11-07 16:10:18 +0800

    Added lunch.json and tasks.json

--- QuickSelect/KthSmallest.ts
+++ QuickSelect/KthSmallest.ts
@@ -4,27 +4,30 @@ export function kthSmallest(
   right: number,
   k: number
 ): number {
-  let pos = partition(arr, left, right);
-  if (pos === k - 1) {
-    return arr[pos];
-  } else if (k - 1 < pos) {
-    return kthSmallest(arr, left, pos - 1, k);
-  } else {
-    return kthSmallest(arr, pos + 1, right, k);
+  if (k > 0 && k <= right - left + 1) {
+    let pos = partition(arr, left, right);
+    if (left == right) return arr[left];
+    if (pos - 1 === k - 1) {
+      return arr[pos];
+    }
+    if (pos - 1 > k - 1) {
+      return kthSmallest(arr, left, pos - 1, k);
+    } else return kthSmallest(arr, pos + 1, right, k - pos + left - 1);
   }
+  console.log("returning zero");
+  return 0;
 }
 
-function partition(arr: Array<number>, start: number = 0, end): number {
+function partition(arr: Array<number>, start: number, end): number {
+  let pivot = arr[end];
   let swapIndex = start;
-  let pivot = arr[start];
-  for (let i = start + 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
+  for (let j = start; j <= end - 1; j++) {
+    if (arr[j] <= pivot) {
+      swap(arr, swapIndex, j);
       swapIndex++;
-      swap(arr, i, swapIndex);
     }
   }
-  swap(arr, swapIndex, start);
-
+  swap(arr, swapIndex, end);
   return swapIndex;
 }
 
@@ -34,6 +37,6 @@ function swap(arr: Array<number>, i: number, j: number) {
   arr[j] = temp;
 }
 
-var arr = [4, 1, 2, 11, -10, -2, -20];
-var r = kthSmallest(arr, 0, arr.length - 1, 1);
+var arr = [4, 1, 2, 11, 21, -11];
+var r = kthSmallest(arr, 0, arr.length - 1, 5);
 console.log(r);
