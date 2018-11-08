7496c99e16f0325e8ee1222cbf809e9f9e9d9f0f
Santosh Kumar Singh <Santosh.Singh@XCHANGING.MY.LOCAL>
2018-11-08 15:02:59 +0800

    Fixed kthSmallest issue

--- QuickSelect/KthSmallest.ts
+++ QuickSelect/KthSmallest.ts
@@ -4,18 +4,17 @@ export function kthSmallest(
   right: number,
   k: number
 ): number {
-  if (k > 0 && k <= right - left + 1) {
-    let pos = partition(arr, left, right);
-    if (left == right) return arr[left];
-    if (pos - 1 === k - 1) {
-      return arr[pos];
-    }
-    if (pos - 1 > k - 1) {
-      return kthSmallest(arr, left, pos - 1, k);
-    } else return kthSmallest(arr, pos + 1, right, k - pos + left - 1);
+  if (left === right) {
+    return arr[left];
   }
-  console.log("returning zero");
-  return 0;
+  let pivot = partition(arr, left, right);
+  if (pivot == k) {
+    return arr[pivot];
+  } else if (k < pivot) {
+    return kthSmallest(arr, left, pivot - 1, k);
+  } else {
+    return kthSmallest(arr, pivot + 1, right, k);
+  }
 }
 
 function partition(arr: Array<number>, start: number, end): number {
@@ -36,7 +35,3 @@ function swap(arr: Array<number>, i: number, j: number) {
   arr[i] = arr[j];
   arr[j] = temp;
 }
-
-var arr = [4, 1, 2, 11, 21, -11];
-var r = kthSmallest(arr, 0, arr.length - 1, 5);
-console.log(r);
